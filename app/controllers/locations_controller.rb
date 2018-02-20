a849b7f1fb82b859ee27cd9985a0db4a4a78fc2e
Luis96791 <luis.fajardozap@hotmail.com>
2018-02-20 01:38:08 -0600

    agregando CRUD de Locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,17 +1,45 @@
 class LocationsController < ApplicationController
 	def new
-
+		@location = Location.new
 	end
 
 	def create
 		@location = Location.new(location_params)
 
-		@location.save
-		redirect_to @location
+		if @location.save
+			redirect_to @location
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@location = Location.find(params[:id])
+	end
+
+	def update
+		@location = Location.find(params[:id])
+
+		if @location.update(location_params)
+    		redirect_to @location
+  		else
+    		render 'edit'
+  		end
+	end
+
+	def index
+  		@locations = Location.all
 	end
 
 	def show
 		@location = Location.find(params[:id])
+	end
+
+	def destroy
+		@location = Location.find(params[:id])
+		@location.destroy
+
+		redirect_to locations_path
 	end
 
 	private
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+	validates :name, presence: true,
+			length: { minimum: 3 }
 end
